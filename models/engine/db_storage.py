dace74c8160d40d14a0d39d3589b95f6d6b789a5
Rashnotech <rashnotech@gmail.com>
2023-10-29 05:31:04 +0000

    Require a review

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -75,11 +75,6 @@ class DBStorage:
         """call remove() method on the private session attribute"""
         self.__session.remove()
 
-    def get(self, cls, id):
-        """retrieve an object """
-        obj = self.__session.query(cls).filter_by(id=id).first()
-        return obj
-
 
     def count(self, cls=None):
         """count the number of objects in storage """
