5d46da35a527fb26b4d9acc224694acb36322ee2
Rashnotech <rashnotech@gmail.com>
2023-10-29 23:00:20 +0000

    Fixed: raising errors

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -5,9 +5,8 @@ from models import storage
 from api.v1.views import app_views, jsonify, request, abort
 
 
-@app_views.route('/amenities', methods=['GET', 'POST'], strict_slashes=False)
-@app_views.route('/amenities/<amenity_id>',
-                 methods=['GET', 'DELETE', 'PUT'], strict_slashes=False)
+@app_views.route('/amenities', methods=['GET', 'POST'])
+@app_views.route('/amenities/<amenity_id>', methods=['GET', 'DELETE', 'PUT'])
 def amenities_list(amenity_id=None):
     """ a function that list amenities """
     if amenity_id is None and request.method == 'GET':
@@ -17,16 +16,16 @@ def amenities_list(amenity_id=None):
     elif request.method == 'POST' and amenity_id is None:
         data = request.get_json()
         if data is None:
-            return jsonify({'error': 'Not a JSON'}), 400
+            abort(400, 'Not a JSON')
         if data.get('name') is None:
-            return jsonify({'error': 'Missing name'}), 400
+            abort(400, 'Missing name')
         new_amenity = Amenity(**data)
         new_amenity.save()
         return jsonify(new_amenity.to_dict()), 201
     else:
         amenity = storage.get(Amenity, amenity_id)
         if amenity is None:
-            return jsonify({'error': 'Not found'}), 404
+            abort(404)
 
         if request.method == 'GET':
             return jsonify(amenity.to_dict()), 200
@@ -34,7 +33,7 @@ def amenities_list(amenity_id=None):
         if request.method == 'PUT':
             data = request.get_json()
             if data is None:
-                return jsonify({'error': 'Not a JSON'}), 400
+                abort(400, 'Not a JSON')
             match = ['id', 'created_at', 'updated_at']
             for key, value in data.items():
                 if key not in match:
