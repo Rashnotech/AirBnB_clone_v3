19e146c8f51e7eae818c20a6a6c25a057276f74f
Rashnotech <rashnotech@gmail.com>
2023-10-29 23:26:02 +0000

    replace state to amenity

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -6,15 +6,13 @@ from api.v1.views import app_views, jsonify, request, abort
 
 
 @app_views.route('/amenities', methods=['GET', 'POST'], strict_slashes=False)
-@app_views.route('/amenities/<amenity_id>', methods=['GET', 'DELETE', 'PUT'],
-                 strict_slashes=False)
-def amenities_list(amenity_id=None):
+def amenities_list():
     """ a function that list amenities """
-    if amenity_id is None and request.method == 'GET':
+    if request.method == 'GET':
         amenities = storage.all(Amenity)
         amenity_list = [amenity.to_dict() for amenity in amenities.values()]
         return jsonify(amenity_list), 200
-    elif request.method == 'POST' and amenity_id is None:
+    if request.method == 'POST':
         data = request.get_json()
         if data is None:
             abort(400, 'Not a JSON')
@@ -23,26 +21,29 @@ def amenities_list(amenity_id=None):
         new_amenity = Amenity(**data)
         new_amenity.save()
         return jsonify(new_amenity.to_dict()), 201
-    else:
-        amenity = storage.get(Amenity, amenity_id)
-        if amenity is None:
-            abort(404)
 
-        if request.method == 'GET':
-            return jsonify(amenity.to_dict()), 200
 
-        if request.method == 'PUT':
-            data = request.get_json()
-            if data is None:
-                abort(400, 'Not a JSON')
-            match = ['id', 'created_at', 'updated_at']
-            for key, value in data.items():
-                if key not in match:
-                    setattr(state, attr, value)
-            storage.save()
-            return jsonify(state.to_dict()), 200
+@app_views.route('/amenities/<aid>', methods=['GET', 'DELETE', 'PUT'],
+                 strict_slashes=False)
+def update_amenities(aid=None):
+    amenity = storage.get(Amenity, aid)
+    if amenity is None:
+        abort(404)
+    if request.method == 'GET':
+        return jsonify(amenity.to_dict()), 200
 
-        if request.method == 'DELETE':
-            storage.delete(amenity)
-            storage.save()
-            return jsonify({}), 200
+    if request.method == 'PUT':
+        data = request.get_json()
+        if data is None:
+            abort(400, 'Not a JSON')
+        match = ['id', 'created_at', 'updated_at']
+        for key, value in data.items():
+            if key not in match:
+                setattr(amenity, attr, value)
+        storage.save()
+        return jsonify(state.to_dict()), 200
+
+    if request.method == 'DELETE':
+        storage.delete(amenity)
+        storage.save()
+        return jsonify({}), 200
